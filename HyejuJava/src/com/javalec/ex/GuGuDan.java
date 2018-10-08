320460743c419d1b95ff01e1d338e1e6adc69029
Hyeju <Hyeju@DESKTOP-OL8PMTB>
2018-10-09 00:30:25 +0900

    설명 추가

--- HyejuJava/src/com/javalec/ex/GuGuDan.java
+++ HyejuJava/src/com/javalec/ex/GuGuDan.java
@@ -14,7 +14,7 @@ public class GuGuDan {
 		guGuDan.gugudan(input);	//gugudan 메소드 호출 객체명.메소드명 
 	}
 	
-	public void gugudan(int i) {
+	public void gugudan(int i) {	//int 타입의 매개변수 하나 받는다. 반환형은 void라서 없다.
 		for(int j=1; j<10; j++) {
 			System.out.println(i + " * " + j + " = " + (i*j));
 		}
