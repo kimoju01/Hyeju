de68b7bbd6931bdfbb080326f64cbcda005c3b9a
Hyeju <Hyeju@DESKTOP-OL8PMTB>
2019-06-21 17:40:53 +0900

    save

--- TestPjt/src/testPjt/Bicycle.java
+++ TestPjt/src/testPjt/Bicycle.java
@@ -9,7 +9,7 @@ public class Bicycle {
 		System.out.println("Bicycle constructor - I");
 	}
 	
-	public Bicycle(String c, int p) {	//생성자를 여러개 둬서 생성과 동시에 객체의 속성을 정할 수 있다.
+	public Bicycle(String c, int p) {	//생성자를 여러개 둬서 생성과 동시에 객체의 속성을 정할 수 있다. 중복 메서드(overloading)
 		System.out.println("Bicycle constructor - II");
 		color = c;	//지금 이 객체의 color에 c를 대입한다.
 					//c라는 것은 이 객체를 생성할 때 생성자를 호출해주는 곳에서(?) 값을 던져준다
