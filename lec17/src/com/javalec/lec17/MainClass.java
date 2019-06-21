9b9b79630460e5733189199a561b69b8ee70ac49
Hyeju <Hyeju@DESKTOP-OL8PMTB>
2019-06-21 18:16:34 +0900

    인프런 17강 데이터 은닉 private, getter, setter

---
+++ lec17/src/com/javalec/lec17/MainClass.java
@@ -0,0 +1,17 @@
+package com.javalec.lec17;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("ȫ�浿", 90);
+		student1.getInfo();
+		
+		student1.setScore(40);
+		student1.getInfo();
+		
+		student1.setScore(100);
+		student1.getInfo();
+		
+	}
+}
