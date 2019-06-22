bc37f1645c88b82f9b4dec20de77df0c28983002
Hyeju <Hyeju@DESKTOP-OL8PMTB>
2019-06-22 22:08:40 +0900

    인프런 26강 파일 입출력 28번째 줄 에러뜬다...

--- lec27/src/com/javalec/lec27_2/MainClass.java
+++ lec27/src/com/javalec/lec27_2/MainClass.java
@@ -22,6 +22,7 @@ public class MainClass {
 			//FileInputStream으로 부터 상위 클래스인 is(InputStream)를 얻어낼 수 있는데, 
 			//is가 byte단위기 때문에 불편해서 문자열 단위로 이용하기 위해 DataInputStream을 이용하는데
 			//즉, is(InputStream)을 DataInputStream의 생성자 매개변수로 넣어서 확장한 개념...?
+			//InputStream을 확장해서 DataInputStream을 만든다..?
 			
 			String str = dis.readUTF();	//문자열 단위로 읽는다.
 			
